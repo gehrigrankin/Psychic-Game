4f3314f12fe812a92f9eb338cf6e3c1fe40356ab
gehrig rankin <rgrankin22@gmail.com>
2018-03-21 15:20:42 -0700

    init commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var userWins = 0;
+var userLosses = 0;
+var comNumber;
+var userGuess;
+var userGuessesLeft = 3;
+
+var guesses = [];
+
+document.onkeyup = function(event) {
+
+    comNumber = Math.floor(Math.random() * 10);
+    console.log(comNumber);
+
+    userGuess = event.key;
+
+    for (var i=0; i< 10; i++){
+        if (userGuess == i){
+            if (userGuess != comNumber) {
+                userGuessesLeft--;
+                guesses.push(userGuess);
+            }
+            if (userGuess == comNumber) {
+                userWins++;
+                userGuessesLeft = 3;
+                guesses = [];
+            }
+
+            if (userGuessesLeft == 0) {
+                userGuessesLeft = 3;
+                userLosses++;
+                guesses = [];
+            }
+
+            document.getElementById("user-text").innerHTML = guesses;
+
+            document.getElementById("guesses-text").innerHTML = userGuessesLeft;
+
+            document.getElementById("wins-text").innerHTML = userWins;
+
+            document.getElementById("losses-text").innerHTML = userLosses;
+        }
+    }
+
+    
+}
